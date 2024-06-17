70d289baa0b95ad437a0b354bc04325c4b3af355
Lydia02 <l.ojoawo@alustudent.com>
2024-06-17 09:56:34 +0100

    dymamic

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,39 +1,59 @@
-const { Sequelize } = require('sequelize');
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    host: process.env.DB_HOST,
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
+const db = {};
+
+let sequelize;
+
+// Conditionally add SSL settings based on the environment
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  const sslConfig = env === 'production' ? {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false // Use this if you are not using a certificate
+      }
+    }
+  } : {};
+
+  sequelize = new Sequelize(config.database, config.username, config.password, {
+    host: config.host,
     dialect: 'postgres',
     pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
     },
-  
-});
-
-const modelDefiners = [
-    require('./user'), // Import the user model definition
-    require('./wasteCollection'),
-    require('./RecyclingEntry'),
-];
-
-// Define all models according to their files.
-for (const modelDefiner of modelDefiners) {
-    modelDefiner(sequelize, Sequelize.DataTypes);
+    ...sslConfig
+  });
 }
 
-// Apply associations if there are any
-// (e.g., if you have relationships like User.hasMany(Posts) you will define them here)
-sequelize.sync(); // You can also handle syncing in a more controlled manner in your app startup
+// Dynamically load models
+fs
+  .readdirSync(__dirname)
+  .filter(file => {
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+  })
+  .forEach(file => {
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    db[model.name] = model;
+  });
 
-const db = {
-    sequelize,
-    Sequelize,
-    User: sequelize.models.User, 
-    WasteCollection: sequelize.models.WasteCollection,
-    RecyclingEntry: sequelize.models.RecyclingEntry,
-};
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
 
 module.exports = db;
