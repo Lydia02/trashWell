14dd96ee04c5c86015683f08e082504f5432e623
Lydia02 <l.ojoawo@alustudent.com>
2024-06-17 08:45:49 +0100

    m

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,27 @@
-require("dotenv").config();
-const express = require("express");
-const { sequelize } = require("./models");
-const authRoutes = require("./routes/auth");
-const scheduleRoutes = require("./routes/schedules");
-const recyclingRoutes = require("./routes/recycling");
-const adminAuth = require("./routes/adminAuth");
-const adminRoutes = require("./routes/admin");
-const cors = require("cors");
+require('dotenv').config();
+const express = require('express');
+const { sequelize } = require('./models');
+const authRoutes = require('./routes/auth');
+const scheduleRoutes = require('./routes/schedules');
+const recyclingRoutes = require('./routes/recycling');
+const adminAuth = require('./routes/adminAuth');
+const adminRoutes = require('./routes/admin'); 
+const cors = require('cors');
+
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static("frontend"));
-app.use("/api/auth", authRoutes);
-app.use("/api/schedules", scheduleRoutes);
-app.use("/api/recycling", recyclingRoutes);
-app.use("/api/admin", adminRoutes);
-app.use("/api/adminauth", adminAuth);
+app.use(express.static('frontend'));
 
-app.get("/", (req, res) => {
-  res.send("Smart Waste Management System is up and running!");
+app.use('/api/auth', authRoutes);
+app.use('/api/schedules', scheduleRoutes);
+app.use('/api/recycling', recyclingRoutes);
+app.use('/api/admin', adminRoutes);
+app.use('/api/adminauth', adminAuth);
+
+app.get('/', (req, res) => {
+    res.send('Smart Waste Management System is up and running!');
 });
+
+
+module.exports = app;
