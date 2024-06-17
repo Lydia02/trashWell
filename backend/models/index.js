d22a4aa69d68b35be2f061fbcb080f0c66e04ad7
Lydia02 <l.ojoawo@alustudent.com>
2024-06-17 08:30:44 +0100

    development

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,32 +1,40 @@
-const Sequelize = require('sequelize');
-const config = require('../config/config'); // Adjust the path if necessary
-const env = process.env.NODE_ENV || 'development';
-const dbConfig = config[env];
+const { Sequelize } = require('sequelize');
+const WasteCollection = require('./wasteCollection');
+const RecyclingEntry = require('./RecyclingEntry');
+require('dotenv').config();
 
-const sequelizeOptions = {
-  host: dbConfig.host,
-  dialect: dbConfig.dialect,
-  port: process.env.DB_PORT || 5432,
-};
-
-// Conditionally add SSL configuration
-if (process.env.DB_USE_SSL === 'true') {
-  sequelizeOptions.dialectOptions = {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
     }
-  };
+});
+
+const modelDefiners = [
+    require('./user'), // Import the user model definition
+    require('./wasteCollection'),
+    require('./RecyclingEntry'),
+];
+
+// Define all models according to their files.
+for (const modelDefiner of modelDefiners) {
+    modelDefiner(sequelize, Sequelize.DataTypes);
 }
 
-const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, sequelizeOptions);
+// Apply associations if there are any
+// (e.g., if you have relationships like User.hasMany(Posts) you will define them here)
+sequelize.sync(); // You can also handle syncing in a more controlled manner in your app startup
 
-const db = {};
-db.Sequelize = Sequelize;
-db.sequelize = sequelize;
-
-db.User = require('./user')(sequelize, Sequelize);
-db.WasteCollection = require('./wasteCollection')(sequelize, Sequelize);
-db.RecyclingEntry = require('./RecyclingEntry')(sequelize, Sequelize);
+const db = {
+    sequelize,
+    Sequelize,
+    User: sequelize.models.User, 
+    WasteCollection: sequelize.models.WasteCollection,
+    RecyclingEntry: sequelize.models.RecyclingEntry,
+};
 
 module.exports = db;
